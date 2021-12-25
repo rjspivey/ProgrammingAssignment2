2eadb1a170044c010f6b645f5aa867c3c2edd53f
rjspivey <rjspivey@users.noreply.github.com>
2021-12-25 14:08:28 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The program inputs a matrix, creates a space for the matrix solution, looks for the matrix solution space, and calculates the matrix's inverse if no solution exists
 
-## Write a short comment describing this function
+## makeCacheMatrix creates the space for calculating the matrix's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+          m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) m <<- solve
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates the matrix's inverse if no inverse currently exist.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
